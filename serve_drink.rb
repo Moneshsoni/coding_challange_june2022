05feb14ecb6b73e06377ec2dc02b52f411ce1b8b
Moneshsoni <sonimonesh115@gmail.com>
2022-06-22 10:12:18 +0530

    add serve drink

---
+++ serve_drink.rb
@@ -0,0 +1,8 @@
+def serve_drink(age)
+    if age>=18 && age<=80
+        puts "You're eligible for drinking"
+    else
+        puts "you're not eligible for drinking"
+    end
+end
+serve_drink(40)
